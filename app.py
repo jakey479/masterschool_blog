607fbe23ef4eb3fd37f5e97f76d82787c091ff80
jakey479 <jacob479321@gmail.com>
2023-09-05 14:24:11 -0400

    update

--- app.py
+++ app.py
@@ -1,12 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
+from json_file_handler_class import JsonFileHandler
+import os
+import json
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates', static_folder='static')
+BLOG_POSTS_FILEPATH = os.path.join('data', 'blog_posts.json')
+# initialize an empty json array to a json file if it is empty or contains invalid json
+try:
+    BLOG_POSTS = JsonFileHandler.return_python_object_from_json(
+        filepath=BLOG_POSTS_FILEPATH
+    )
+except json.decoder.JSONDecodeError as e:
+    BLOG_POSTS = []
+    JsonFileHandler.write_python_object_to_json(
+        python_object=BLOG_POSTS,
+        filepath=BLOG_POSTS_FILEPATH
+    )
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def return_blog_posts():
+    return render_template(
+        'display_blog_posts.html',
+        blog_posts=BLOG_POSTS
+    )
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run('0.0.0.0', port=5000, debug=True)
--- json_file_handler_class.py
+++ json_file_handler_class.py
@@ -2,14 +2,14 @@ import json
 
 
 class JsonFileHandler:
-    def __init__(self, filepath: str):
-        self.filepath = filepath
 
-    def write_python_object_to_json(self, python_object):
-        with open(self.filepath, 'w') as fileobj:
+    @staticmethod
+    def write_python_object_to_json(filepath: str, python_object):
+        with open(filepath, 'w') as fileobj:
             json.dump(python_object, fileobj, indent=4)
 
-    def return_python_object_from_json(self, json_filepath):
-        with open(json_filepath) as fileobj:
+    @staticmethod
+    def return_python_object_from_json(filepath: str):
+        with open(filepath) as fileobj:
             python_object = json.load(fileobj)
         return python_object
